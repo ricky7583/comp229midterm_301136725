74ab4b28b97c11372dbc88f6d188e183c92d1ab1
ricky7583 <ricky7583@naver.com>
2021-10-29 14:27:08 -0400

    GET the Book Details page in order to add a new Book

--- server/routes/books.js
+++ server/routes/books.js
@@ -23,14 +23,7 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-  let emptyBook = book({
-    "title":"",
-    "description":"",
-    "Price":"",
-    "Author":"",
-    "Genre":""
-  });
-  res.render('books/details', {title: 'Add Book info', books: emptyBook});
+  res.render('books/details', {title: 'Add Book info', books: ''});
 });
 
 // POST process the Book Details page and create a new Book - CREATE
